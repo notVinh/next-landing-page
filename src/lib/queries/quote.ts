c230ca78db9db179e7514ac35c61bbd53f06ae8d
notVinh <dqv4work@gmail.com>
2026-02-24 11:40:23 +0700

    first commit 4

--- src/lib/queries/quote.ts
+++ src/lib/queries/quote.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const backendUrl = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api`;
 
-export const sendQuoteRequest = async (formData) => {
+export const sendQuoteRequest = async (formData: any) => {
   const data = await axios.post(
     `${backendUrl}/quotations/public/submit`,
     formData,
